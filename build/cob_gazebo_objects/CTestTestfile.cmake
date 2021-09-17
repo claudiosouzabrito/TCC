ec1750b5509cb614124060357b1ed564d602ea45
claudio <claudiosouzabrito@gmail.com>
2021-09-17 00:03:27 -0300

    bookshelf

---
+++ build/cob_gazebo_objects/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cloud/ROS/TCC/src/cob_gazebo_objects
+# Build directory: /home/cloud/ROS/TCC/build/cob_gazebo_objects
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
